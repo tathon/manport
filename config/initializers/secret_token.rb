dfb14f24d8b24ecda4c7b2f61babaa6c1a9f6782
Chu Tat Hon <chutathon@yahoo.com>
2011-01-19 19:54:10 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manport::Application.config.secret_token = '8f4977953b3e04247aac38feaa76e0e588460293beaa96ddc2717c933db78e294c8150b5898152f00d60547e51a14817331f08deea8c9b3b38b663844dc4207e'
